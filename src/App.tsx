28ed6cfcc048eec98ba0e63945efa71fe469c908
liuwanyong <liuwanyong2018@gmail.com>
2023-02-16 15:20:01 +0800

    模拟医保支付宝

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,53 @@
  * @Author: liuwanyong liuwanyong2018@gmail.com
  * @Date: 2022-10-15 22:14:11
  * @LastEditors: liuwanyong liuwanyong2018@gmail.com
- * @LastEditTime: 2022-10-16 02:13:36
+ * @LastEditTime: 2023-02-16 13:43:54
  * @FilePath: /react-study/src/App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEgo1
  */
 
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { DownFill } from "antd-mobile-icons";
+import {Card } from 'antd-mobile'
 import './App.css'
-import useSWR from 'swr'
+
 
 function App() {
-   const [count, setCount] = useState(0)
-   const f: (args: RequestInfo | URL) => Promise<Response> = (...args) => {
-      return fetch(...args).then(res => res.json())
-   }
-   const { data, error } = useSWR('https://awwfl9o2d6mcfeqioi6oelhfnrd2.requestly.me/1/?delay=30', f)
-   console.log(data, error?.message)
+
+
 
    return (
       <div className="App">
-         <div>
-            <a href="https://vitejs.dev" target="_blank">
-               <img src="/logo1.svg" className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://reactjs.org" target="_blank">
-               <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+         <img src="/4.jpg" alt="" />
+         <div className="select">
+            <div className="box">
+               <span>2023</span>
+               <DownFill color="#000" fontSize={'.5em'} />
+            </div>
          </div>
-         <h1>Vite + React</h1>
          <div className="card">
-            <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-            <p>
-               Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
+            <Card>
+               <div className="content">
+                  <header>
+                     <span>2023年1月</span>
+                     <p>
+                        <span className='light'>共缴纳（元）：</span>
+                        <span className="num">637.79</span>
+                     </p>
+                  </header>
+                  <main>
+                     <span>险种类型：</span>
+                     <span>职工基本医疗保险</span>
+                     <div className="line"></div>
+                  </main>
+                  <div className="more">展开</div>
+               </div>
+            </Card>
+            <p className='nom'>没有更多了</p>
          </div>
-         <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+         <footer>
+            <img src="/5.jpg" alt="" />
+         </footer>
       </div>
    )
 }
